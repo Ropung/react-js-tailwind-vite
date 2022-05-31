7f157fbc684842d603a11bd6653e9134b7f25626
Gihun <563710@naver.com>
2022-05-31 17:08:49 +0900

    Feat(Route): Apply routing.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {" "}
+        {/* Switch~case 같은 역할 */}
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
