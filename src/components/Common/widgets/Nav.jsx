8dc2d7b0fd03794d00689cf769913410ef832453
Gihun <563710@naver.com>
2022-05-31 18:19:18 +0900

    Feat : Add link tag to header Nav.

---
+++ src/components/Common/widgets/Nav.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">홈으로</Link>
+        </li>
+        <li>
+          <Link to="/login">로그인</Link>
+        </li>
+        <li>
+          <Link to="/todolist">투두리스트</Link>
+        </li>
+        <li>
+          <Link to="/ui-preview">ui-preview</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
